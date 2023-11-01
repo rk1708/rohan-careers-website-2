13bbf78a2b872306ef2ae2655ab427fe8f83e316
rk1708 <38866658+rk1708@users.noreply.github.com>
2023-11-01 12:29:02 +0000

    add dynamic pages for each job

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template, jsonify
-from database import engine
+from database import engine, load_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
 
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs=[]
-    for row in result.all():
-      jobs.append(dict(row._mapping))
-    return jobs
   
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template("home.html", jobs=jobs, company='Rohan')
+  return render_template("home.html", jobs=jobs)
 
 
-@app.route("/jobs")
+@app.route("/api/jobs")
 def list_jobs():
   jobs= load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found", 404
+  return render_template("jobpage.html", job=job)
+  
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -3,3 +3,17 @@ import os
 
 db_connection_string= os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string, connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}})
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs=[]
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    row = result.fetchone()
+    return row._asdict()
